750424fca72e7a104b51054dea42335ff6604e64
amlaku <amlakubeyene@gmail.com>
2022-06-05 21:17:31 +0300

    divisible

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-def delete_at(my_list=[], idx=0):
-    if idx < 0:
-        return my_list
-    my_list.remove(my_list[idx])
-    return my_list
+def divisible_by_2(my_list=[]):
+    check_div = []
+
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            check_div.append(True)
+        else:
+            check_div.append(False)
+
+    return (check_div)
