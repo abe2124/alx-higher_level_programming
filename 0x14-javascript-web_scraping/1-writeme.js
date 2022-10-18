c8b0f3960dbab6efe7cd7d450f9cec1ad91c9d4c
Amlaku Beyene <amlakubeyene@gmail.com>
2022-10-18 14:15:47 +0300

    javascript web

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,13 +1,5 @@
 #!/usr/bin/node
-/**
- * writes a string to a file.
- * first argument is the file path
- * content of the file must be written in utf-8
- */
-
 const fs = require('fs');
-const file = process.argv[2];
-const phrasal = process.argv[3];
-fs.writeFile(file, phrasal, 'utf8', function (err) {
-  if (err) throw err;
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
